1317cdebce22f9a211ac1acc54614e4ba7677a69
Utkarsh Nigam <webxthedev@gmail.com>
2024-12-06 09:15:01 +0530

    Fixed routing part

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,25 @@
 'use client'
 
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/Home/Home'
 import VideoInteration from './components/VideoInteraction/VideoInteration'
 
 export default function App() {
-  
+
 
   return (
     <>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/videoupload" element={<VideoInteration />} />
-        </Routes>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/videoupload" element={<VideoInteration />} />
+          </Routes>
+        </Router>
       </div>
 
     </>
   )
 
-  
+
 }
