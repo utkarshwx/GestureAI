9ce28b180a0978092d50da45e67103fe7d6425f0
Utkarsh Nigam <webxthedev@gmail.com>
2024-12-06 16:47:08 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home/Home'
 import VideoInteration from './components/VideoInteraction/VideoInteration'
 
@@ -10,12 +10,11 @@ export default function App() {
   return (
     <>
       <div className="App">
-        <Router>
+        
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/videoupload" element={<VideoInteration />} />
           </Routes>
-        </Router>
       </div>
 
     </>
